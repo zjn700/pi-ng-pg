064817b1f63af3b3a6161cd40eccc0f15f58c8f8
zjn700 <zjn700-01@yahoo.com>
2022-03-21 20:09:57 +0800

    fix for hostListener

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ShellComponent } from './shared/shell/shell.component';
 // import { CommonModule } from '@angular/common';
 
 const routes: Routes = [
-
+  {path: '', component: ShellComponent}
 ]
 
 @NgModule({
